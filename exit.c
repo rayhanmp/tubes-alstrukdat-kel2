920d3007f6929042780214c35e6125a0f471e830
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-01 16:27:26 +0700

    Create exit.c

---
+++ exit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exit(int status){
+    printf("Anda keluar dari game BNMO.\n");
+    printf("Bye bye...\n");
+}
+
+
+int main()
+{
+// ini test buat manggil exit yh
+    exit(0);
+    return 0;
+}
