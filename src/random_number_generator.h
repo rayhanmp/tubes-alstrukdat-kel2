ba59db4c28515c14f37fb3362f6d44ab58969d40
Iyaq1 <93713795+Iyaq1@users.noreply.github.com>
2022-11-04 09:23:44 +0700

    Delete random_number_generator.h

--- src/random_number_generator.h
+++
@@ -1,8 +0,0 @@
-/* random_number_generator.h*/
-
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
-
-unsigned long rng();
-/* Mengembalikan angka acak dari 0 sampai 4.294.967.295 dengan menggambil seed dari time dan clock.*/
