a8beea848541bb8eeec1d8d75ffdf80172a197db
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-09 15:02:08 +0700

    Create commandlain.h

---
+++ src/Commands/commandlain.h
@@ -0,0 +1,12 @@
+#ifndef _CMDLAIN_H_
+#define _CMDLAIN_H_
+
+#include <stdio.h>
+
+void cmdLain();
+/*
+I.S.: Sembarang
+F.S.: Program mengeluarkan output (print) yakni "Command tidak dikenali, silahkan masukkan command yang valid"
+*/
+
+#endif
