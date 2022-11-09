3523ede62871009610c3cc7fd2c24d565b0881ef
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-10 00:56:56 +0700

    Create queueGame.h

---
+++ src/Commands/queueGame.h
@@ -0,0 +1,12 @@
+#ifndef _PROSEDUR_QUEUE_GAME_H_
+#define _PROSEDUR_QUEUE_GAME_H_
+
+#include "../ADT/queueGame.h"
+
+void QueueGame(queueGame *q);
+/*
+I.S.: queueGame sembarang
+F.S.: queueGame memiliki tambahan antrean berdasarkan masukan pengguna dari daftar gim
+*/
+
+#endif
