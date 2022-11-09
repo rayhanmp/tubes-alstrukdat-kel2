53f348f7483dab0bb8bf9710414f57c2a2e06fbe
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-10 01:12:54 +0700

    Update deletegame.c
    Perubahan metode input dari scanf menjadi STARTWORD_INPUT()

--- src/Commands/deletegame.c
+++ src/Commands/deletegame.c
@@ -1,23 +1,20 @@
 #include "deletegame.h"
 
-void cmpStr (char *a, char *b) {
-    // masih butuh metode compare inputnya
-    while ()
-}
-
-void deleteGame (arrGame *arr, queueGame queue) {
+void deleteGame (arrGame *arr, queueGame q) {
     int val;
     printf("Berikut adalah daftar game yang tersedia\n");
     PrintArray(*arr);
     printf("\n");
 
     printf("Masukkan nomor game yang akan dihapus: ");
-    scanf("%d", &val);
+    clearWord();
+    STARTWORD_INPUT();
+    val = wordToInt(currentWord);
 
     if (val>5) {
-        if (!IsMember(queue)) {
-                if(cmpStr) { // Masih butuh metode compare
-                    DeleteAt(arr, val);
+        if (!IsMember(q)) {
+                if(!(isKataEqual(currentWord, "RNG")) && !(isKataEqual(currentWord, "Diner Dash"))) {
+                    DeleteAt(&arr, val);
                     printf("Game berhasil dihapus\n");
                     return 0;
                 }
