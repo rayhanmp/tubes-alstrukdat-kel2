fa379f8ee5cd431fd38c68616d43c1a9cfd83551
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-09 22:26:06 +0700

    Create quit.h

---
+++ src/Commands/quit.h
@@ -0,0 +1,13 @@
+#ifndef _QUIT_H_
+#define _QUIT_H_
+
+#include "../ADT/mesinkata.h"
+#include "../ADT/queueGame.h"
+
+void quit (queueGame queue);
+/*
+I.S.: Sembarang
+F.S.: queueGame kosong
+*/
+
+#endif
