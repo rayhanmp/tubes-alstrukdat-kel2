f6897551de8baeefb30317ed5ee34f9388af0057
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-10 02:00:23 +0700

    Update and rename exit.c to quit.c

--- src/Commands/quit.c
+++ src/Commands/quit.c
@@ -5,10 +5,6 @@ int main(){
     int flag = 0;
     char ch[100];
     do{
-        printf("ENTER COMMAND: ");
-        scanf("%s", &ch);
-    
-        if (strcmp(ch, "QUIT") == 0)
             printf("Anda keluar dari game BNMO.\n");
             printf("Bye bye...\n");
             flag = 1;
