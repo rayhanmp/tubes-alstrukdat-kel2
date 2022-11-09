16e2423b3bef344b283334d4362eb2e390ad2cae
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-10 01:08:02 +0700

    Update queueGame.h
    Revisi parameter input

--- src/Commands/queueGame.h
+++ src/Commands/queueGame.h
@@ -2,8 +2,9 @@
 #define _PROSEDUR_QUEUE_GAME_H_
 
 #include "../ADT/queueGame.h"
+#include "../ADT/arrGame.h"
 
-void QueueGame(queueGame *q);
+void QueueGame(queueGame *q, arrGame arr) {
 /*
 I.S.: queueGame sembarang
 F.S.: queueGame memiliki tambahan antrean berdasarkan masukan pengguna dari daftar gim
