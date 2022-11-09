1b2e056f002cb4d4f16385f84b446d1ec1fccf4b
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-10 01:14:32 +0700

    Update deletegame.h
    Perubahan nama parameter input

--- src/Commands/deletegame.h
+++ src/Commands/deletegame.h
@@ -5,7 +5,7 @@
 #include "../ADT/arrayGame.h"
 #include "../ADT/queueGame.h"
 
-void deleteGame (arrGame *arr, queueGame queue);
+void deleteGame (arrGame *arr, queueGame q);
 /*
 I.S.: arrGame sembarang
 F.S.: Elemen arrGame tertentu dihapus apabila game tidak terdapat dalam queue, bukan 5 game pertama, dan merupakan game custom pengguna
