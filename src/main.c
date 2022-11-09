189b7152dfa63b5dc0927e998e1f947a78b061c4
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-11-09 22:34:43 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,40 @@
+#include "ADT/mesinkata.h"
+#include "ADT/mesinkarakter.h"
+#include "Commands/load.h"
+#include "Commands/save.h"
+#include "ADT/arrayGame.h"
+
 int main(){
+    //INISIALISASI ARRAY GAME
+    arrGame arr;
+    MakeArray(&arr);
+
+    //MAIN PROGRAM
+    printf(" /$$$$$$$  /$$   /$$  /$$$$$$  /$$      /$$  /$$$$$$ \n");
+    printf("| $$__  $$| $$$ | $$ /$$__  $$| $$$    /$$$ /$$__  $$\n");
+    printf("| $$  \\ $$| $$$$| $$| $$  \\ $$| $$$$  /$$$$| $$  \\ $$\n");
+    printf("| $$$$$$$ | $$ $$ $$| $$  | $$| $$ $$/$$ $$| $$  | $$\n");
+    printf("| $$__  $$| $$  $$$$| $$  | $$| $$  $$$| $$| $$  | $$\n");
+    printf("| $$  \\ $$| $$\\  $$$| $$  | $$| $$\\  $ | $$| $$  | $$\n");
+    printf("| $$$$$$$/| $$ \\  $$|  $$$$$$/| $$ \\/  | $$|  $$$$$$/\n");
+    printf("|_______/ |__/  \\__/ \\______/ |__/     |__/ \\______/ \n");
+
+    while(true){
+        clearWord();
+        printf("MASUKKAN COMMAND: ");
+        STARTWORD_INPUT();
+        if(isKataEqual(currentWord,"SAVE")){
+            ADVWORD_INPUT();
+            save(&arr, currentWord.TabWord);
+        }
+        else if(isKataEqual(currentWord,"LOAD")){
+            ADVWORD_INPUT();
+            load(&arr, currentWord.TabWord);
+            }
+        else{
+            printf("command tidak tersedia.\n");
+        }
+    }
     return 0;
 }
+
