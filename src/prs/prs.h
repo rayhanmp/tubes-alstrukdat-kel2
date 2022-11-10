cbeaba7a9468f5e37b64c2e2e69c72414b9ba0d1
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-10 17:37:31 +0700

    Create prs.h
    File untuk gim Paper, Rock, Scissor

---
+++ src/prs/prs.h
@@ -0,0 +1,12 @@
+#ifndef __PRS_H__
+#define __PRS_H__
+
+#include "mesinkata.h"
+#include "arrayGame.h"
+
+void prs();
+/*  I.S. Sembarang
+    F.S. Program menuliskan skor lawan dan skor pengguna serta  pemenang gim Paper Rock Scissor
+*/
+
+#endif
