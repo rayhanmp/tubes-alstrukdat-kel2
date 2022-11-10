4b1e3695ef595f1aa753e95122175e2eda027189
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-10 16:17:40 +0700

    Update quit.c

--- src/Commands/quit.c
+++ src/Commands/quit.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+void quit(){
     int flag = 0;
-    char ch[100];
     do{
             printf("Anda keluar dari game BNMO.\n");
             printf("Bye bye...\n");
             flag = 1;
         
-            while (!IsEmpty(Q)) {
-                Eltype val;
-                Dequeue(&Q, val);
+            while (!IsEmptyQ(Q)) {
+                Eltype Game;
+                Dequeue(&Q, Game);
             }
                 
         }
