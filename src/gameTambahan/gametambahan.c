af4a85a6a1a3dd5611a6f6c07e219e0593904aaa
marchelinefanni <92165974+marchelinefanni@users.noreply.github.com>
2022-11-10 11:47:33 +0700

    gametambahan

---
+++ src/gameTambahan/gametambahan.c
@@ -0,0 +1,7 @@
+#include "gametambahan.h"
+
+void gametambahan(){
+    long long skor = rng(1,1000); // min max?
+    printf("GAME OVER!\n");
+    printf("Skor Akhir: %lld\n", skor);
+}
---
+++ src/gameTambahan/gametambahan.h
@@ -0,0 +1,13 @@
+#ifndef _GAMETAMBAHAN_H_
+#define _GAMETAMBAHAN_H_
+
+#include "../diner_dash/random_number_generator.h"
+
+void gametambahan();
+/*
+I.S.: sembarang
+F.S.: Game langsung selesai dan masuk ke tahap game over
+      dengan skor akhir berupa integer random
+*/
+
+#endif
