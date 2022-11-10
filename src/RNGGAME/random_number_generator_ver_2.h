ae31a58fb74b082926eedb733293e9d2e960c746
Iyaq1 <93713795+Iyaq1@users.noreply.github.com>
2022-11-10 19:48:37 +0700

    Add files via upload

--- src/RNGGAME/random_number_generator_ver_2.h
+++ src/RNGGAME/random_number_generator_ver_2.h
@@ -6,8 +6,14 @@
 #include <math.h>
 
 void seedrngver2(long long *seed);
+/* I.S seed terdefinisi
+   F.S seed berisi seed
+Menaruh seed ke output*/
 long long rngver2(long long *seed ,long long min, long long max);
-/* Mengembalikan angka acak dari min sampai max dengan menggambil seed dari time dan clock.
-NOTES : Jangan gunakan fungsi ini berturut-turut dengan waktu yang pendek, 
-minimal berikan waktu kurang lebih 1 milidetik sebelum memanggil fungsi ini lagi.*/
+/* Mengembalikan angka acak dari min sampai max dengan menggambil seed dari yang sudah tersedia dan mengubah seed menjadi yang berbeda.*/
+/* HOW TO USE
+   Pertama, jalankan prosedur seedrngver2 terlebih dahulu dengan cara :seedrngver2(&seed). PENTING : Jangan jalankan seedrngver2 di awal program. 
+   Kedua gunakan rngver2 sesuai kepentingan dengan cara : rngver2(&seed, min, max). Dengan argumen adalah sesuai dengan argumen pada seedrngver2, dan kedua argumen
+   setelahnya adalah parameter min dan max.
+*/
 #endif
