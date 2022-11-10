582162297a066e0f2e92a60e0e398ef3297b7cec
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-11-10 21:39:02 +0700

    Update creategame.c

--- src/Commands/creategame.c
+++ src/Commands/creategame.c
@@ -1,6 +1,13 @@
 #include "creategame.h"
 
 void createGame (arrGame *arr){
+    Game game;
+    int jumlah_game;
+    jumlah_game = wordToInt(Get(*arr,0));
+    jumlah_game++;
+    game = intToWord(jumlah_game);
+    DeleteFirst(arr);
+    InsertFirst(arr, game);
     printf("Masukkan nama game yang akan ditambahkan: ");
     STARTWORD_INPUT();
     InsertLast(arr, currentWord);
