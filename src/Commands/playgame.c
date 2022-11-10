3980ea835af4ff23e84247b679a8e314236f15a8
marchelinefanni <92165974+marchelinefanni@users.noreply.github.com>
2022-11-10 13:34:03 +0700

    revisi playgame

--- src/Commands/playgame.c
+++ src/Commands/playgame.c
@@ -1,26 +1,30 @@
 #include "playgame.h"
 
-void playGame (arrGame arr, queueGame* q){
+void playGame (queueGame* q){
     printf("Berikut adalah daftar game yang tersedia\n");
     PrintQueue(*q);
     printf("\n");
 
     Word game;
     Dequeue(q, &game);
-    if (IsMember(arr, game)){ // IsMember dari array
-        printf("Loading %s ...\n", game.TabWord);
-        if (isKataEqual(game, "Diner DASH")){
-            diner_dash();
-        } else if (isKataEqual(game, "RNG")){
-            rnggame();
-        } else {
-            gametambahan();
-        }
+    if (IsEmptyQ){
+        printf("Daftar Game kosong\n");
     } else {
-        printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", game.TabWord);
-        printf("Silahkan pilih game lain.\n");
+        if (isKataEqual(game, "DINOSAUR IN EARTH") || isKataEqual(game, "RISEWOMAN") || isKataEqual(game, "EIFFEL TOWER")){
+            printf("Game %s masih dalam maintenance, belum dapat dimainkan.\n", game.TabWord);
+            printf("Silahkan pilih game lain.\n");
+        } else {
+            printf("Loading %s ...\n", game.TabWord);
+            if (isKataEqual(game, "Diner DASH")){
+                diner_dash();
+            } else if (isKataEqual(game, "RNG")){
+                rnggame();
+            } else {
+                gametambahan();
+            }
+        }
     }
-}
+
 /*
 I.S.: q terdefinisi, currentGame Nil (?)
 F.S.: q berkurang satu, currentGame adalah elemen pertama q sebelum dihapus
--- src/Commands/playgame.h
+++ src/Commands/playgame.h
@@ -7,7 +7,7 @@
 #include "../RNGGAME/rnggame.h"
 #include "../gameTambahan/gametambahan.h"
 
-void playGame (arrGame arr, queueGame* q);
+void playGame (queueGame* q);
 /*
 I.S.: q terdefinisi, currentGame Nil (?)
 F.S.: q berkurang satu, currentGame adalah elemen pertama q sebelum dihapus
