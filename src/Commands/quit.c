27accaa9d05d77a8074e0a3fec245c1cb7c985a6
jazmy <jazmyizzati11@gmail.com>
2022-11-11 09:05:45 +0700

    Update quit.c

--- src/Commands/quit.c
+++ src/Commands/quit.c
@@ -3,18 +3,13 @@
 
 void quit(queueGame queue){
     int flag = 0;
-    do{
             printf("Anda keluar dari game BNMO.\n");
             printf("Bye bye...\n");
             flag = 1;
         
-            while (!IsEmptyQ(Q)) {
-                Eltype Game;
-                Dequeue(&Q, Game);
+            while (!IsEmptyQ(queue)) {
+                Game Game;
+                Dequeue(&queue, &Game);
             }
-                
-        }
-    while (flag == 0);
 
-    return 0;
     }
