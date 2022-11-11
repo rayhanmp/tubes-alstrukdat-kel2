94b8bde0e980af666f4e8724df381e211b7561a5
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-11-11 20:24:41 +0700

    Update driver_mesinkata.c

--- src/ADT/Driver/driver_mesinkata.c
+++ src/ADT/Driver/driver_mesinkata.c
@@ -4,6 +4,8 @@ int main(){
     //MEMINTA MASUKKAN
     STARTWORD_INPUT();
     printf("masukan:");
+    printf(" %s", currentWord.TabWord);
+    ADVWORD_INPUT();
     //JIKA MASUKKAN LEBIH DARI 1 KATA
     while(!isEndWord()){
         printf(" %s", currentWord.TabWord);
