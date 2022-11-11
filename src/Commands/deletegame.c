95aa3c77b86e87b1624e9eb02e64ed838d724c15
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2022-11-11 08:25:09 +0700

    Penambahan komentar dan fix typo

--- src/Commands/deletegame.c
+++ src/Commands/deletegame.c
@@ -2,23 +2,25 @@
 
 void deleteGame (arrGame *arr, queueGame q) {
     int val;
+    /* Tuliskan daftar gim yang tersedia kepada pengguna */
     printf("Berikut adalah daftar game yang tersedia\n");
     PrintArray(*arr);
     printf("\n");
 
+    /* Minta masukan kepada pengguna */
     printf("Masukkan nomor game yang akan dihapus: ");
-    clearWord();
     STARTWORD_INPUT();
     val = wordToInt(currentWord);
 
-    if (val>5) {
-        if (!IsMember(q)) {
-                if(!(isKataEqual(currentWord, "RNG")) && !(isKataEqual(currentWord, "Diner Dash"))) {
-                    DeleteAt(&arr, val);
+    /* Cek kondisi */
+    if (val>5) { // Jika bukan 5 gim pertama
+        if (!IsMemberQ(q)) { // Jika gim tidak terdapat di queueGame
+                if(!(isKataEqual(currentWord, "RNG")) && !(isKataEqual(currentWord, "Diner Dash"))) { // Jika gim bukan bawaan sistem (RNG/Diner Dash)
+                    DeleteAt(arr, val);
                     printf("Game berhasil dihapus\n");
-                    return 0;
+                    return 0; // Early return agar printf di bawahnya tidak dieksekusi
                 }
         }
     }
-    printf("Game gagal dihapus\n");
+    printf("Game gagal dihapus\n"); // Hanya dieksekusi apabila salah satu dari if statement di atas bernilai false
 }
