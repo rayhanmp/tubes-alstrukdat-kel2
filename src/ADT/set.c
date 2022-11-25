da318c040a1d51987793a044223b597ab9fdbaf2
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-25 16:43:55 +0700

    Update set.c

--- src/ADT/set.c
+++ src/ADT/set.c
@@ -69,3 +69,69 @@ boolean IsMember(Set S, infotype Elmt){
 	return found;
 }
 /* Mengembalikan true jika Elmt adalah member dari S */
+
+Set SetUnion(Set s1, Set s2){
+    int i;
+	Set s3; 
+    CreateEmpty(&s3);
+	for (i = 0; i < s1.Count; i++){
+		if (!IsMember(s3, s1.Elements[i])){
+            Insert(&s3, s1.Elements[i]);
+	}
+    }
+	for (i = 0; i < s2.Count; i++) {
+		if (!IsMember(s3, s2.Elements[i])){
+            Insert(&s3, s2.Elements[i]);
+        }
+	}
+	return s3;
+}
+// Mengembalikan set baru yang berisi elemen-elemen yang terdapat pada s1 atau s2
+// Contoh: [1, 2] U [2, 3] = [1, 2, 3]
+
+Set SetIntersection(Set s1, Set s2){
+    Set s3; 
+    CreateEmpty(&s3);
+	for (int i = 0; i < s1.Count; i++){
+		if (IsMember(s2, s1.Elements[i])){
+            Insert(&s3, s1.Elements[i]);
+        }
+	}
+	return s3;
+}
+// Mengembalikan set baru yang berisi elemen-elemen dari s1 dan s2 yang terdapat pada kedua set
+// Contoh: [1, 2] ∩ [2, 3] = [2]
+
+Set SetSymmetricDifference(Set s1, Set s2){
+    int i;
+	Set s3; 
+    CreateEmpty(&s3);
+	for (i = 0; i < s1.Count; i++){
+		if (!IsMember(s2, s1.Elements[i])){
+            Insert(&s3, s1.Elements[i]);
+        }
+	}
+	for (i = 0; i < s2.Count; i++){
+		if (!IsMember(s1, s2.Elements[i])){
+            Insert(&s3, s2.Elements[i]);
+        }
+	}
+	return s3;
+}
+// Mengembalikan set baru yang berisi elemen yang ada di s1 atau s2, tapi tidak pada keduanya
+// Contoh: [1, 2] ⊖ [2, 3] = [1, 3]
+
+Set SetSubtract(Set s1, Set s2){
+    Set s3; 
+    CreateEmpty(&s3);
+	for (int i = 0; i < s1.Count; i++){
+		if (!IsMember(s2, s1.Elements[i])){
+            Insert(&s3, s1.Elements[i]);
+        }
+	}
+	return s3;
+}
+// Mengembalikan set baru yang berupa hasil pengurangan s1 dengan s2
+// Contoh:
+// s1 = [1, 2] s2 = [2, 3]
+// s1 - s2 = [1]
