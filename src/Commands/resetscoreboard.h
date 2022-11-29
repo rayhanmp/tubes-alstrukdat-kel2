cbe854046c3ba82e08cb6a955d69705990aec7b5
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-29 22:55:25 +0700

    Update resetscoreboard.h

---
+++ src/Commands/resetscoreboard.h
@@ -0,0 +1,12 @@
+#ifndef _RESETSCOREBOARD_H_
+#define _RESETBOARD_H_
+
+#include <stdio.h>
+#include "scoreboard.h"
+#include "../ADT/mesinkarakter.h"
+
+void resetScoreboard(Map RNG, Map dinerDash, Map hangman, Map tower, Map snake, arrGame Games);
+/*I.S. Sembarang*/
+/*F.S. Menghapus semua data dan atau data yang diinginkan pada scoreboard*/
+
+#endif
