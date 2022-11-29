2a81ca0221e2fcc1ae8fd1b277afada9975686d4
marchelinefanni <92165974+marchelinefanni@users.noreply.github.com>
2022-11-29 20:21:18 +0700

    stack game

---
+++ src/ADT/stackGame.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackGame.h"
+
+void CreateEmpty(stackGame *S){
+    Top(*S) = NilSG;
+}
+/* I.S. sembarang{} */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(stackGame S){
+    return (Top(S) == NilSG);
+}
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsFull(stackGame S){
+    return (Top(S) == MaxSG-1);
+}
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(stackGame * S, Game game){
+    Top(*S) += 1;
+    InfoTop(*S) = game;
+}
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(stackGame * S, Game* game){
+    *game = InfoTop(*S);
+    Top(*S) -= 1;
+}
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
---
+++ src/ADT/stackGame.h
@@ -0,0 +1,62 @@
+/* File : stackGame.h */
+/* deklarasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
+/* TOP adalah alamat elemen puncak */
+/* Implementasi dalam bahasa C dengan alokasi statik */
+#ifndef stackGame_H
+#define stackGame_H
+
+#include <stdio.h>
+#include "../boolean.h"
+#include "word.h"
+#include "mesinkata.h"
+
+#define NilSG -1
+#define MaxSG 100
+/* Nil adalah stack dengan elemen kosong . */
+
+typedef Word Game;
+typedef int address;   /* indeks tabel */
+
+/* Contoh deklarasi variabel bertype stack dengan ciri TOP : */
+/* Versi I : dengan menyimpan tabel dan alamat top secara eksplisit*/
+typedef struct { 
+  Game T[MaxSG]; /* tabel penyimpan elemen */
+  address TOP;  /* alamat TOP: elemen puncak */
+} stackGame;
+/* Definisi stack S kosong : S.TOP = Nil */
+/* Elemen yang dipakai menyimpan nilai Stack T[0]..T[MaxEl-1] */
+/* Jika S adalah Stack maka akses elemen : */
+   /* S.T[(S.TOP)] untuk mengakses elemen TOP */
+   /* S.TOP adalah alamat elemen TOP */
+
+/* Definisi akses dengan Selektor : Set dan Get */
+#define Top(S) (S).TOP
+#define InfoTop(S) (S).T[(S).TOP]
+
+/* ************ Prototype ************ */
+/* *** Konstruktor/Kreator *** */
+void CreateEmpty(stackGame *S);
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(stackGame S);
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+boolean IsFull(stackGame S);
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(stackGame * S, Game game);
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(stackGame * S, Game* game);
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+
+#endif
