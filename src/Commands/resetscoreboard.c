781124fc929f0a900238ceca3bae4f1888b1e0e7
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-29 23:17:53 +0700

    Update resetscoreboard.c

--- src/Commands/resetscoreboard.c
+++ src/Commands/resetscoreboard.c
@@ -1,19 +1,71 @@
 #include "resetscoreboard.h"
 
-void resetScoreboard(scoreMap RNG, scoreMap dinerDash, scoreMap hangman, scoreMap tower, scoreMap snake, arrGame Games){
-    int i = 0;
-    while(i < Length(Games)){
-        if(isKataEqual(Get(Games,i),"HANGMAN")){
-            CreateEmptyMap(&hangman);
-        }
-        if(isKataEqual(Get(Games,i),"TOWER OF HANOI")){
-            CreateEmptyMap(&tower);
-        }
-        if(isKataEqual(Get(Games,i),"SNAKE ON METEOR")){
-            CreateEmptyMap(&snake);
-        }
-        if(isKataEqual(Get(Games,i),"DINER DASH")){
+void resetScoreboard(Map RNG, Map dinerDash, Map hangman, Map tower, Map snake, arrGame Games){
+    printf("DAFTAR SCOREBOARD:\n");
+    printf("0. ALL\n");
+    for (int i = 1; i <= Length(Games); i++){
+        printf("%d. %s\n",i,Get(Games,i).TabWord);
+    }
+
+    printf("SCOREBOARD YANG INGIN DIHAPUS:  \n");
+    STARTWORD_INPUT();
+    if (isKataEqual(currentWord,"0")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SEMUA SCOREBOARD? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&RNG);
             CreateEmptyMap(&dinerDash);
+            CreateEmptyMap(&hangman);
+            CreateEmptyMap(&tower);
+            CreateEmptyMap(&snake);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
+        }
+    } else if (isKataEqual(currentWord,"1")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SCOREBOARD RNG? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&RNG);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
+        }
+    } else if (isKataEqual(currentWord, "2")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SCOREBOARD DINER DASH? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&dinerDash);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
+        }
+    } else if (isKataEqual(currentWord, "3")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SCOREBOARD HANGMAN? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&hangman);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
+        }
+    } else if (isKataEqual(currentWord, "4")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SCOREBOARD TOWER OF HANOI? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&tower);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
+        }
+    } else if (isKataEqual(currentWord, "5")){
+        printf("APAKAH ANDA YAKIN INGIN MENGHAPUS SCOREBOARD SNAKE ON METEOR? (YA/TIDAK)\n");
+        STARTWORD_INPUT();
+        if (isKataEqual(currentWord,"YA")){
+            CreateEmptyMap(&snake);
+            printf("Scoreboard berhasil di-reset\n");
+        } else if (isKataEqual(currentWord,"TIDAK")){
+            printf("SCOREBOARD TIDAK DI-RESET\n");
         }
     }
 }
