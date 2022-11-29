df1d49b10304163c2b19c9d64462b89df72cca67
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-11-29 20:36:00 +0700

    Upadate matrixchar

--- src/ADT/matrixchar.c
+++ src/ADT/matrixchar.c
@@ -6,27 +6,31 @@
 
 
 void CreateEmptyMatrixChar(matrixchar *m,int h, int w){
+
     for(int i = 0; i < h; i++){
         for(int j = 0; j < w; j++){
-            m->info[i][j] = ' ';
+            InsertLastW(&m->info[i][j], "  ");
         }
     }
     m->h = h;
     m->w = w;
 }
 
-void InsertMatrixChar(matrixchar *m, char c, point p){
-    m->info[Yp(p)][Xp(p)] = c;
+void InsertMatrixChar(matrixchar *m, Word c, point p){
+    CloneWord(&m->info[Yp(p)][Xp(p)], c);
+    // InsertLastW(&m->info[Yp(p)][Xp(p)], c);
+    // m->info[Yp(p)][Xp(p)] = c;
 }
 
 void PrintMatrixChar(matrixchar m){
     for(int i = 0; i < m.h; i++){
         for(int j = 0; j < m.w; j++){
-            printf("|%c",m.info[i][j]);
+            printf("|");
+            printWord(m.info[i][j]);
         }
         printf("|\n|");
         for(int j = 0; j < m.w; j++){
-            printf("_|");
+            printf("__|");
         }
         printf("\n");
         }
--- src/ADT/matrixchar.h
+++ src/ADT/matrixchar.h
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "point.h"
-
+#include "word.h"
 typedef struct
 {
-    char info[20][20];
+    Word info[20][20];
     int h;
     int w;
 } matrixchar;
@@ -13,7 +13,7 @@ typedef struct
 void CreateEmptyMatrixChar(matrixchar *m, int h, int w);
 /* I.S. m sembarang  */
 /* F.S. Terbentuk matrixchar kosong. Dimana isi dari info dari 0,0 sampai h,w berisi ' '. */
-void InsertMatrixChar(matrixchar *m, char c, point p);
+void InsertMatrixChar(matrixchar *m, Word c, point p);
 /* I.S. m terdefinisi*/
 /* F.S. m[Yp(p)][Xp(p)] adalah c*/
 void PrintMatrixChar(matrixchar m);
