fb3cda11d830ccf5f247fcc2667ed17b2bc91393
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-29 21:41:52 +0700

    Add files via upload

---
+++ src/Commands/scoreboard.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "../ADT/map.h"
+
+void printScoreboard(){
+    int i = 0;
+	while(i < Length(Games)){
+		if(isKataEqual(Get(Games,i),"HANGMAN")){
+			printf("***SCOREBOARD HANGMAN***\n");
+			printf("|   NAMA    |   SKOR   |\n");
+			printf("-----------------------\n");
+            PrintMap(hangman);
+		}
+        if(isKataEqual(Get(Games,i),"TOWER OF HANOI")){
+			printf("***SCOREBOARD TOWER OF HANOI***\n");
+			printf("|   NAMA    |   SKOR   |\n");
+			printf("-----------------------\n");
+            PrintMap(tower);
+		}
+        if(isKataEqual(Get(Games,i),"SNAKE ON METEOR")){
+			printf("***SCOREBOARD SNAKE ON METEOR***\n");
+			printf("|   NAMA    |   SKOR   |\n");
+			printf("-----------------------\n");
+            PrintMap(snake);
+		}
+        if(isKataEqual(Get(Games,i),"DINER DASH")){
+			printf("***SCOREBOARD DINER DASH***\n");
+			printf("|   NAMA    |   SKOR   |\n");
+			printf("-----------------------\n");
+            PrintMap(dinerDash);
+		}
+    }
+}
+/*I.S. Sembarang*/
+/*F.S. Menampilkan scoreboard yang berisi nama dan skor pemain yang telah bermain*/
