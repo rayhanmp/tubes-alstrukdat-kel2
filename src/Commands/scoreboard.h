e5a410e7f48f8cb1dce94acd5ef95ef422444366
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-11-29 22:43:25 +0700

    Update scoreboard.h

--- src/Commands/scoreboard.h
+++ src/Commands/scoreboard.h
@@ -1,4 +1,11 @@
+#ifndef _SCOREBOARD_H_
+#define _SCOREBOARD_H_
+
 #include <stdio.h>
 #include "../ADT/map.h"
 
 void printScoreboard(Map Creategame, Map RNG, Map dinerDash, Map hangman, Map tower, Map snake, arrGame Games);
+/*I.S. Sembarang*/
+/*F.S. Menampilkan scoreboard yang berisi nama dan skor pemain yang telah bermain*/
+
+#endif
