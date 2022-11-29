05bc710b36b9bc2b34689c17b058cdd1c39838c0
Jazmy Izzati Alamsyah <88818677+Mipol2@users.noreply.github.com>
2022-11-29 22:14:17 +0700

    Update Command scoreboard

--- src/Commands/scoreboard.c
+++ src/Commands/scoreboard.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-#include "../ADT/map.h"
+#include "scoreboard.h"
 
-void printScoreboard(){
+void printScoreboard(scoreMap Creategame, scoreMap RNG, scoreMap dinerDash, scoreMap hangman, scoreMap tower, scoreMap snake, arrGame Games){
     int i = 0;
 	while(i < Length(Games)){
 		if(isKataEqual(Get(Games,i),"HANGMAN")){
 			printf("***SCOREBOARD HANGMAN***\n");
-			printf("|   NAMA    |   SKOR   |\n");
-			printf("-----------------------\n");
+			printf("|     NAMA      |  SKOR    |\n");
+			printf("----------------------------\n");
             PrintMap(hangman);
 		}
         if(isKataEqual(Get(Games,i),"TOWER OF HANOI")){
 			printf("***SCOREBOARD TOWER OF HANOI***\n");
-			printf("|   NAMA    |   SKOR   |\n");
-			printf("-----------------------\n");
+			printf("|     NAMA      |  SKOR    |\n");
+			printf("----------------------------\n");
             PrintMap(tower);
 		}
         if(isKataEqual(Get(Games,i),"SNAKE ON METEOR")){
 			printf("***SCOREBOARD SNAKE ON METEOR***\n");
-			printf("|   NAMA    |   SKOR   |\n");
-			printf("-----------------------\n");
+			printf("|     NAMA      |  SKOR    |\n");
+			printf("----------------------------\n");
             PrintMap(snake);
 		}
         if(isKataEqual(Get(Games,i),"DINER DASH")){
 			printf("***SCOREBOARD DINER DASH***\n");
-			printf("|   NAMA    |   SKOR   |\n");
-			printf("-----------------------\n");
+			printf("|     NAMA      |  SKOR    |\n");
+			printf("----------------------------\n");
             PrintMap(dinerDash);
 		}
     }
---
+++ src/Commands/scoreboard.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "../ADT/map.h"
+
+void printScoreboard(Map Creategame, Map RNG, Map dinerDash, Map hangman, Map tower, Map snake, arrGame Games);
