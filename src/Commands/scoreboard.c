8bd1010f6f91957acfd49b365f0fb539ade8bea9
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-12-02 03:54:25 +0700

    Update scoreboard.c

--- src/Commands/scoreboard.c
+++ src/Commands/scoreboard.c
@@ -1,31 +1,4 @@
 #include "scoreboard.h"
-
-void sortScoreboard(Map m3, boolean key_based){
-	Map m4; 
-    	CreateEmpty(&m4);
-	int loop = m3.Count;
-	for (i = 0; i < loop; i++) {
-		int keyMin = m3.Elements[0].Key;
-		int valMin = m3.Elements[0].Value;
-		for (j = 0; j < m3.Count; j++) {
-			if (key_based) {
-				if (m3.Elements[j].Key < keyMin) {
-					keyMin = m3.Elements[j].Key;
-					valMin = m3.Elements[j].Value;
-				}
-			} else {	
-				if (m3.Elements[j].Value < valMin) {
-					keyMin = m3.Elements[j].Key;
-					valMin = m3.Elements[j].Value;
-				}
-			}
-		}
-		Insert(&m4, keyMin, valMin);
-		Delete(&m3, keyMin);
-	}
-	return m4;
-}
-/* Masih copas-an */
 	
 void printScoreboard(arrScore Scores, arrGame Games){
     int i = 1;
