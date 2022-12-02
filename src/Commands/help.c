c6808dca5289fb61141024e3e34b2a1e225f58b5
miraculouscat <108720558+miraculouscat@users.noreply.github.com>
2022-12-02 19:36:18 +0700

    update help.c

--- src/Commands/help.c
+++ src/Commands/help.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "help.h"
 
-void help(){
-  printf("===========================NINUNINUNINU===========================\n");
-  printf("===============================HELP===============================\n");
-  printf("===========================NINUNINUNINU===========================\n");
+void help(){  
+  printf("██╗░░██╗███████╗██╗░░░░░██████╗░\n");
+  printf("██║░░██║██╔════╝██║░░░░░██╔══██╗\n");
+  printf("███████║█████╗░░██║░░░░░██████╔╝\n");
+  printf("██╔══██║██╔══╝░░██║░░░░░██╔═══╝░\n");
+  printf("██║░░██║███████╗███████╗██║░░░░░\n");
+  printf("╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░░░░\n");
+  printf("\nWelcome to the help menu!\n");
+  printf("Here are the available commands:\n");
   printf("1. START - membaca file konfigurasi sistem yang berisi list game yang dapat dimainkan\n");
   printf("2. LOAD - membaca save file yang berisi list game yang dapat dimainkan, histori, dan scoreboard game \n");
   printf("3. SAVE - menyimpan kondisi state game pemain terkini \n");
@@ -14,7 +19,11 @@ void help(){
   printf("7. QUEUEGAME - mendaftarkan game ke dalam list\n");
   printf("8. PLAYGAME - memainkan sebuah permainan\n");
   printf("9. SKIPGAME - melewatkan permainan sebanyak n \n");
-  printf("10. QUIT - keluar dari game\n");
-  printf("11. HELP - berisi bantuan atas command-command yang ada\n");
-  printf("12. COMMAND LAIN - input command tidak valid atau tidak terdapat \n");
+  printf("10. SCOREBOARD - menampilkan nama pemain dan score\n");
+  printf("11. RESET SCOREBOARD - mengosongkan data pemain dan score pada scoreboard\n");
+  printf("12. HISTORY - menampilkan riwayat permainan yang pernah dimainkan\n");
+  printf("13. RESET HISTORY - mengosongkan riwayat game yang dimainkan pemain\n");
+  printf("14. QUIT - keluar dari game\n");
+  printf("15. HELP - berisi bantuan atas command-command yang ada\n");
+  printf("16. COMMAND LAIN - input command tidak valid atau tidak terdapat \n");
 }
